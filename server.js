eb837fc5937a936a67ded9fbbf682f30da07b655
Raquel Dionísio <dionisio@stadtwerk.org>
2022-06-21 17:24:03 +0200

    using middleware

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+
+const logger = (req, res, next) => {
+    console.log("Log")
+    next()
+    // "next" is a function that you call so the next piece of middleware runs
+}
+
+app.use(logger)
+
 app.get("/", (req, res) => {
     res.send("Homepage")
+    console.log("Homepage")
+
 })
 
 app.get("/users", (req, res) => {
+    console.log("Users Page")
     res.send("Users Page")
 })
+
+
 
 app.listen(PORT, console.log("Server listening on port 3000"))
 
