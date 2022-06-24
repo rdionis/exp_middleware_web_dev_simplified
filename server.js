96c465d3f56249356ee15acd6587627ee8a5a2c3
Raquel Dionísio <dionisio@stadtwerk.org>
2022-06-24 12:11:33 +0200

    adding auth middleware

--- server.js
+++ server.js
@@ -4,20 +4,34 @@ const PORT = 3000
 
 
 const logger = (req, res, next) => {
-    console.log("Log")
+    console.log("Log", req.originalUrl)
     next()
     // "next" is a function that you call so the next piece of middleware runs
 }
+
+const auth = (req, res, next) => {
+    req.query.admin === "true" ? next() : res.send("No Auth")
+    // if (req.query.admin === "true"){
+    //     next()
+    // } else {
+    //     res.send("No auth")
+        // we are never calling next in the failure case of our authentication
+    //}
+    // console.log("Auth")
+    // next()
+}
+
+// when you are creating global actions you almost always want to put them at the very top so they'll run before everything else
 
 app.use(logger)
 
 app.get("/", (req, res) => {
     res.send("Homepage")
     console.log("Homepage")
-
+    
 })
 
-app.get("/users", (req, res) => {
+app.get("/users", auth, (req, res) => {
     console.log("Users Page")
     res.send("Users Page")
 })
@@ -26,3 +40,4 @@ app.get("/users", (req, res) => {
 
 app.listen(PORT, console.log("Server listening on port 3000"))
 
+// middleware runs in the order that you define
